5708892493c10628444c700e2eb04b7b75df3ecf
winnieliuzzz <winnieliuzzz@gmail.com>
2018-05-29 20:56:23 -0700

    web_blog

--- web_blog/src/models/blog.py
+++ web_blog/src/models/blog.py
@@ -5,8 +5,9 @@ from src.models.post import Post
 
 
 class Blog:
-    def __init__(self, author, title, description, _id=None):
+    def __init__(self, author, title, description, author_id, _id=None):
         self.author = author
+        self.author_id = author_id
         self.title = title
         self.description = description
         self._id = uuid.uuid4().hex if _id is None else _id
@@ -35,6 +36,7 @@ class Blog:
     def json(self):
         return {
             'author': self.author,
+            'author_id': self.author_id,
             'title': self.title,
             'description': self.description,
             '_id': self._id
@@ -45,3 +47,9 @@ class Blog:
         blog_data = Database.find_one(collection='blogs',
                                       query={'_id':id})
         return cls(**blog_data)
+
+    @classmethod
+    def find_by_author_id(cls, author_id):
+        blogs = Database.find(collection='blogs',
+                              query={'author_id':author_id})
+        return [cls(**blog) for blog in blogs]
--- web_blog/src/models/user.py
+++ web_blog/src/models/user.py
@@ -1,6 +1,10 @@
+import datetime
 import uuid
 
+from flask import session
+
 from src.common.database import Database
+from src.models.blog import Blog
 
 
 class User:
@@ -22,10 +26,10 @@ class User:
             return cls(**data)
 
     @staticmethod
-    def login_valid(self, email, password):
+    def login_valid(email, password):
         user = User.get_by_email(email)
         if user is not None:
-            return password == password
+            return user.password == password
         return False
 
     @classmethod
@@ -34,18 +38,42 @@ class User:
         if user is None:
             new_user = cls(email, password)
             new_user.save_to_mongo()
+            session['email'] = email
             return True
         else:
             return False
 
-    def login(self):
-        pass
+    @staticmethod
+    def login(user_email):
+        session['email'] = user_email
+
+    @staticmethod
+    def logout(user_email):
+        session['email'] = None
 
     def get_blogs(self):
-        pass
+        return Blog.find_by_author_id(self._id)
+
+    def new_blog(self, title, description):
+        blog = Blog(author=self.email,
+                    title=title,
+                    description=description,
+                    author_id=self._id)
+        blog.save_to_mongo()
+
+    @staticmethod
+    def new_post(blog_id, title, content, date=datetime.datetime.utcnow()):
+        blog = Blog.from_mongo(blog_id)
+        blog.new_post(title=title,
+                      content=content,
+                      date=date)
 
     def json(self):
-        pass
+        return {
+            "email": self.email,
+            "_id": self._id,
+            "password": self.password
+        }
 
     def save_to_mongo(self):
-        pass
+        Database.insert("users", self.json())
